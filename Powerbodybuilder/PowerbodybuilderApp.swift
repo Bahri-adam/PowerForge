4c3b67e240ad215648f79ee9eca3ceb536b29a1b
Bahri-adam <bahri@umich.edu>
2026-03-04 09:44:00 -0500

    Initial Commit

---
+++ Powerbodybuilder/PowerbodybuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerbodybuilderApp.swift
+//  Powerbodybuilder
+//
+//  Created by Adam Bahri on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PowerbodybuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerbodybuilderTests/PowerbodybuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PowerbodybuilderTests.swift
+//  PowerbodybuilderTests
+//
+//  Created by Adam Bahri on 3/4/26.
+//
+
+import Testing
+@testable import Powerbodybuilder
+
+struct PowerbodybuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
